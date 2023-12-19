092415dd22d24d2f765f79e75758a84280617bf6
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-19 21:57:28 +0500

    a

--- static/cdn/static_cdn/webuni/js/checkout.js
+++ static/cdn/static_cdn/webuni/js/checkout.js
@@ -1,6 +1,6 @@
 // Create a Stripe client.
 
-var stripe = Stripe('');
+var stripe = Stripe('pk_test_LI02rx6BCdiFgRYQbCaU28o0');
 
   // Create an instance of Elements.
   var elements = stripe.elements();
