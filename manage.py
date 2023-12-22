a91b1cfb7151ead0bbb61d05e6823e91f2066c6c
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-22 23:23:28 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import os
 import sys
 
@@ -8,6 +7,5 @@ if __name__ == '__main__':
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-
         ) from exc
     execute_from_command_line(sys.argv)
