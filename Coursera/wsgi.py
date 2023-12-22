d596bc131120ae57db553a3f7f43d603c35ae610
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-22 23:17:29 +0500

    Update wsgi.py

--- Coursera/wsgi.py
+++ Coursera/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for Coursera project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
 
 import os
 
