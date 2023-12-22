cae705dd3a847815e5825eb2505f7f9bb9f16650
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-22 22:46:25 +0500

    a

--- memberships/views.py
+++ memberships/views.py
@@ -77,13 +77,9 @@ def PaymentView(request):
 
 
         customer = stripe.Customer.retrieve(user_membership.stripe_customer_id)
-        customer.source = token # 4242424242424242
+        customer.source = token 
         customer.save()
 
-        '''
-        Now we can create the subscription using only the customer as we don't need to pass their
-        credit card source anymore
-        '''
 
         subscription = stripe.Subscription.create(
             customer=user_membership.stripe_customer_id,
